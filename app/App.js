255392e6cc07dc77ea25e426f8a0ce0c9050e809
elponchis <1124jisoo@gmail.com>
2026-06-05 14:33:49 +0900

    Migrate to native-stack navigator + dev server config
    - Replace @react-navigation/stack with @react-navigation/native-stack for Expo Go compatibility
    - Add react-native-gesture-handler and @react-navigation/elements deps
    - Update BASE_URL in api.js (NOTE: change to your Raspberry Pi IP before running)

--- app/App.js
+++ app/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen   from './screens/HomeScreen';
 import AuthScreen   from './screens/AuthScreen';
 import EnrollScreen from './screens/EnrollScreen';
 import AdminScreen  from './screens/AdminScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
